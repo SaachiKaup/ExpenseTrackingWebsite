d096ab7e0a2c0089b000b8531aaac14b38f213fa
SaachiKaup <saachi.kaup@gmail.com>
2022-05-25 23:15:14 +0530

    Removed IDs folder from pages/src and moved to root directory

---
+++ IDs/setu.js
@@ -0,0 +1,4 @@
+//does not export default react component, could be used later
+const client_id = "d6d5aca0-8b79-46de-8ac2-0f572be84273"
+const client_secret = "b45f3b49-06de-42f5-a816-3e137427c406"
+export default [client_id, client_secret]
