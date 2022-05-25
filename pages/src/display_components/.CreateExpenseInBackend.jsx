d123ceda5d86ab10efc997677ead81b2a19ed6c3
SaachiKaup <saachi.kaup@gmail.com>
2022-05-25 16:29:35 +0530

    Added Create/Delete Expense In backend back to git

---
+++ pages/src/display_components/.CreateExpenseInBackend.jsx
@@ -0,0 +1,34 @@
+export default async function CreateExpenseInBackend(expense_inserted, category, categories, expense_date = false) {
+    if(expense_date === false) {
+        expense_date = new Date();
+    }
+    else {
+        expense_date = new Date(expense_date);
+    }
+    const axios = require('axios');
+    console.log('in creation function: ', expense_inserted);
+    console.log('category while creating: ', category)
+    console.log("date while creating: ", expense_date)
+    const base_url = 'http://localhost:3000';
+    const expense_data = {
+        "expenses": {
+            "user_id": 4,
+            "cat_id": categories.indexOf(category) + 1,
+            "daily_amt": Number(expense_inserted),
+            "expense_date": expense_date
+        }
+    };
+    console.log('expense_data', expense_data);
+    axios.post(base_url + '/api/expenses/create', expense_data).then(
+        res => {
+            console.log('res data', res.data);
+        }
+    )
+        .catch(err => {
+            console.log(err);
+        }
+        );
+    return null || expense_data;
+}
+
+//export default CreateExpenseInBackend;
---
+++ pages/src/display_components/.DeleteExpenseInBackend.jsx
@@ -0,0 +1,20 @@
+export default async function DeleteExpenseInBackend(expense_id) {
+    const axios = require('axios');
+    console.log('in deletion function: ', typeof expense_id);
+
+    const base_url = 'http://localhost:3000';
+    const expense_data = {
+        "expense_id": expense_id
+    };
+    console.log('expense_data', expense_data);
+    axios.post(new URL(base_url + '/api/expenses/delete'), expense_data).then(
+        res => {
+            console.log('res data', res.data);
+        }
+    )
+        .catch(err => {
+            console.error('error data', err);
+        }
+        );
+    return null || expense_data; //expense_data || null
+}
