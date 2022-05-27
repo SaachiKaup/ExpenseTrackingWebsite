0a22ace01837b77ecb2c3e51b3ab02db848f2b8d
SaachiKaup <saachi.kaup@gmail.com>
2022-05-27 23:27:09 +0530

    Made handleSubmit code more functional

--- pages/InputNumberForTransactions.js
+++ pages/InputNumberForTransactions.js
@@ -17,13 +17,32 @@ const get_local_storage_item = (item_name) => {
     return window.localStorage.getItem(item_name)
 }
 
+const get_cropped_consent_id_from_url = (req_url) => {
+    return req_url.slice(req_url.indexOf('webview/') + 8, req_url.length)
+}
+
+const get_consent_id = (userMobileNo) => {
+    const url_redirect_promise = get_promise('/api/setu/consent/', userMobileNo)
+    let consent_id = url_redirect_promise.then(res => {
+        console.log('Promise Response: ', res)
+        let req_url = res.data
+        console.log('URL in Promise', req_url)
+        const consent_id_from_url = get_cropped_consent_id_from_url(req_url)
+        return consent_id_from_url}).catch(err => err)
+    return consent_id
+}
+
+const get_consent_with_status_and_mobile_number_from_local_storage = () => {
+    const local_storage_consentID = get_local_storage_item('consentID')
+    const local_storage_consentStatus = get_local_storage_item('consentStatus')
+    const local_storage_userMobileNo = get_local_storage_item('userMobileNo')
+    return [local_storage_consentID, local_storage_consentStatus, local_storage_userMobileNo]
+}
 
 function InputNumberForTransactions() {
     const [userMobileNo, setUserMobileNo] = useState('')
     const [btnDisabled, setBtnDisabled] = useState(true)
     const [consentStatus, setConsentStatus] = useState('') //consentStatus would be a better name for consentStatus
-
-
 
     const handleChange = (event) => {
         let event_target_value = event.target.value
@@ -44,10 +63,12 @@ function InputNumberForTransactions() {
 
     const handleSubmit = (event) => {
         console.log("Submit event Fired with event: ", event)
-        const local_storage_userMobileNo = get_local_storage_item('userMobileNo')
-        const local_storage_consentID = get_local_storage_item('consentID')
-        const local_storage_consentStatus = get_local_storage_item('consentStatus')
+        const [local_storage_userMobileNo, 
+                local_storage_consentID, 
+                local_storage_consentStatus] = 
+        get_consent_with_status_and_mobile_number_from_local_storage()
         console.log("local storage number: ", local_storage_userMobileNo, "id: ", local_storage_consentID, "status: ", local_storage_consentStatus)
+        
     }
 
     return (
