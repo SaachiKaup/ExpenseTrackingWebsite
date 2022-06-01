c62295c9cf6afa78cb5fb80520c079e0d29787d3
SaachiKaup <saachi.kaup@gmail.com>
2022-06-01 15:24:54 +0530

    Changed Config to see if api works on deployment

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/setu/check_consent/[consent_id].js',
+            destination: 'https://expense-tracking-website-git-master-saachikaup.vercel.app/api/setu/consent/[consent_id].js',
+          },
+        ]
+      },
+  };
