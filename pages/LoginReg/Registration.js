cd7858772accdc6ea9f0742967013b919427dd8c
SaachiKaup <saachi.kaup@gmail.com>
2022-06-02 11:16:32 +0530

    added a basic form with name input

--- pages/LoginReg/Registration.js
+++ pages/LoginReg/Registration.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
+import { useForm } from 'react-hook-form'
 
 function Registration() {
+  const { register } = useForm();
   return (
-    <div>Registration</div>
+    <form>
+      <div>
+        <p>Form Testing</p>
+        <label htmlFor="name">Name</label>
+        <input  {...register("name", { required: true})}/>
+      </div>
+    </form>
   )
 }
 
 export default Registration
+
+/* <div>
+        <label htmlFor="name">Name</label>
+        <input type = "text" name="name" id = "name" ref={register} />
+      </div> 
+      {console.log("Reg: ", register)} */
