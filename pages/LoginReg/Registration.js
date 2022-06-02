94caeab4ee419981bf89fa2967faf494db99fee3
SaachiKaup <saachi.kaup@gmail.com>
2022-06-02 11:29:01 +0530

    Added All form Values

--- pages/LoginReg/Registration.js
+++ pages/LoginReg/Registration.js
@@ -1,16 +1,30 @@
 import React, {useState, useEffect, useRef} from 'react'
 import { useForm } from 'react-hook-form'
+import Card from '../src/shared/Card'
+import Button from '../src/shared/Button'
 
 function Registration() {
   const { register } = useForm();
   return (
+    <Card>
     <form>
       <div>
-        <p>Form Testing</p>
-        <label htmlFor="name">Name</label>
+        <label htmlFor="name">  Name  </label>
         <input  {...register("name", { required: true})}/>
       </div>
+      <div>
+        <label htmlFor="email">  Email  </label>
+        <input  {...register("email", { required: true})}/>
+      </div>
+      <div>
+        <label htmlFor="mobile_number">  Mobile Number </label>
+        <input  {...register("mobile_number", { required: true})}/>
+      </div>
+      <div>
+        <button type = "submit">Register</button>
+      </div>
     </form>
+    </Card>
   )
 }
 
