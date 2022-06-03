417540e430f63a1d9b5946338d3ed19ac3413f95
SaachiKaup <saachi.kaup@gmail.com>
2022-06-03 17:24:22 +0530

    Made changes to create users, starts from Num 15

--- pages/api/users/create.tsx
+++ pages/api/users/create.tsx
@@ -1,25 +1,27 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
+let current_user_id = 15;
 
 export default async function create_users(req: NextApiRequest, res: NextApiResponse) {
     const prisma = new PrismaClient({log: ["query"]});
-    
-    
+    console.log("Current user_ID: " + current_user_id);
     try {
         const {users: userData} = req.body
         const user = await prisma.users.create({
-        data: {
-            user_id: userData.user_id,
-            name: userData.name,
-            email: userData.email,
-            mobile_number: userData.mobile_number,
-        },
+            data: {
+                user_id: current_user_id,
+                name: userData.name,
+                email: userData.email,
+                mobile_number: userData.mobile_number,
+            },
         });
     
         res.status(201).json(user);
+        current_user_id++;
     } catch (error) {
         res.status(500).json({ error: error.message });
     } finally {
         await prisma.$disconnect();
     }
 }
+
