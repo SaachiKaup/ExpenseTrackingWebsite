17178c38b4bc1c69fdaba40714e3b5f2c2978082
SaachiKaup <saachi.kaup@gmail.com>
2022-06-04 17:33:18 +0530

    CreateUserInBackendFromAPI added, default userID = 22 for now

---
+++ api_call/CreateUserInBackendFromAPI.jsx
@@ -0,0 +1,37 @@
+export async function CreateUserInBackendFromAPI(user_data_json) {
+    const axios = require('axios');
+    console.log('api_call/ in user creation function: ', expense_inserted);
+    //const base_url = 'http://localhost:3000';
+    const base_url = 'https://expense-tracking-website-git-master-saachikaup.vercel.app'
+    const user_data = JSON.stringify({
+        "users": {
+            "user_id": 22, 
+            "name": user_data_json.name,
+            "email": user_data_json.email,
+            "mobile_number": user_data_json.mobile_number,
+        }
+    });
+    var config = {
+        method: 'post',
+        url: '/api/users/create',
+        headers: { 
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': "*"
+        },
+        data : user_data
+    };
+    console.log('user_data', user_data);
+    //axios.post(base_url + '/api/expenses/create', expense_data).then(
+    axios(config).then(
+        axios_res => {
+            console.log('axios res data', axios_res.data);
+        }
+    )
+        .catch(err => {
+            console.log(err);
+        }
+        );
+    return user_data || null;
+}
+
+//export default CreateExpenseInBackend;
