621118818d67915be82bb6477e731137e4a4c20d
SaachiKaup <saachi.kaup@gmail.com>
2022-06-12 20:17:35 +0530

    Added Login button, removed unnecessary imports from index

--- pages/LoginReg/Login.js
+++ pages/LoginReg/Login.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function Login() {
+  const { loginWithRedirect } = useAuth0()
+
   return (
-    <div>Login</div>
+    <button onClick={() => { 
+      loginWithRedirect()
+    }}>Log In</button>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Categories from './src/display_components/Categories'
-import InputExpense from './src/display_components/InputExpense'
-import App from './App'
-import Link from 'next/link'
-import Card from './src/shared/Card'
 import MainMenu from './MainMenu'
 import { Auth0Provider } from '@auth0/auth0-react'
 
@@ -17,7 +9,7 @@ export default function Home() {
     <Auth0Provider
       domain = {domain}
       clientId = {clientId}
-      redirectUri = {'/'}
+      redirectUri = {'/'} //window.location.origin worked before
     >
       <MainMenu />
     </Auth0Provider>
