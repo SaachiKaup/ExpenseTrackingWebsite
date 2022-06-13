8587e032c7550471a7c0e58221158f9681cea359
SaachiKaup <saachi.kaup@gmail.com>
2022-06-13 15:30:52 +0530

    Added UserProvider Wrapper Component in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
+import { UserProvider } from '@auth0/nextjs-auth0'
 //import App from './App' :Not required
 
 function MyApp({ Component, pageProps }) {
-  return (<Component {...pageProps} />)
+  return (
+  <UserProvider>
+    <Component {...pageProps} />
+  </UserProvider>  
+  )
 }
 
 export default MyApp
