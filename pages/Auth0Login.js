a307ec9a641ee47015e828c7c77b83c519417b66
SaachiKaup <saachi.kaup@gmail.com>
2022-06-13 11:18:20 +0530

    "added Auth0Login page to import login component"

---
+++ pages/Auth0Login.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import Login from "./LoginReg/Login";
+
+function Auth0Login() {
+    return (
+        <>
+        <Login />
+        </>
+    )
+}
+
+export default Auth0Login
