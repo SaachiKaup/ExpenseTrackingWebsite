b509e42cdfc398869e83e53252596169ede472ba
SaachiKaup <saachi.kaup@gmail.com>
2022-06-13 14:26:09 +0530

    Removed React auth from index.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,7 @@
 import MainMenu from './MainMenu'
-import { Auth0Provider } from '@auth0/auth0-react'
-
-const domain = process.env.REACT_APP_AUTH0_DOMAIN
-const clientId = process.env.REACT_APP_AUTH0_CLIENTID
 
 export default function Home() {
   return (
-    <Auth0Provider
-      domain = {domain}
-      clientId = {clientId}
-      redirectUri = {'/'} //window.location.origin worked before
-    >
       <MainMenu />
-    </Auth0Provider>
   )
 }
