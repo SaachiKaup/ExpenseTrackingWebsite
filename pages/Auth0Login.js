274578eb939fe50a34e5a1b2d6137cb098077114
SaachiKaup <saachi.kaup@gmail.com>
2022-06-30 11:26:27 +0530

    Added Changes to LoginPage Styling

--- pages/Auth0Login.js
+++ pages/Auth0Login.js
@@ -1,12 +1,22 @@
 import React, {useState} from 'react';
 import Login from "./LoginReg/Login";
 import Link from 'next/link';
+import styles from '../styles/Home.module.css'
+import Card from './src/shared/Card';
+import Header from './src/display_components/Header';
 
 function Auth0Login() {
     return (
-        <>
-        <Link href = "/api/auth/login">Log In </Link>
-        </>
+        <div className={styles.container}>
+            <Card>
+                <div className={styles.title}>
+                <Header text = "Expense Tracker App"/>
+                </div>
+                <div  className={styles.grid}>
+                    <Link href = "/api/auth/login"><u>Log In</u></Link>
+                </div>
+            </Card>
+        </div>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,25 @@ import Auth0Login from './Auth0Login'
 // import Link from 'next/link'
 import { useUser } from '@auth0/nextjs-auth0'
 import Menu from './MainMenu.js'
+import Card from './src/shared/Card';
 
 export default function Home() {
   const { user, error, isLoading } = useUser();
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
+
+  if (isLoading) 
+    return (
+      <Card>
+        <div>Loading...</div>
+      </Card>
+    );
+
+  if (error) 
+    return (
+      <Card>
+        <div>{error.message}</div>
+      </Card>
+    );
+    
   if (user) {
     return (
       <>
